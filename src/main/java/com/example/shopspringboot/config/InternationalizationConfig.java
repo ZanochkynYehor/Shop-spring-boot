4ed56000b2f356bad4a512c6ea1495672aaa80a1
Yehor Zanochkyn <egor.zanochkin9@gmail.com>
2022-04-10 15:08:02 +0300

    change SessionLocaleResolver to CookieLocaleResolver

--- src/main/java/com/example/shopspringboot/config/InternationalizationConfig.java
+++ src/main/java/com/example/shopspringboot/config/InternationalizationConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -27,8 +27,8 @@ public class InternationalizationConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
-        return slr;
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.ENGLISH);
+        return clr;
     }
 }
